f57408430bec8bf8ed11358bf0f7a07f3bc49d9b
d <vf>
2023-10-07 19:20:04 +0500

    Число

---
+++ task00/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+0. Напишите программу, которая на вход принимает число и выдает его квадрат
+(число умноженное на само себя).
+*/
+System.Console.WriteLine ("Введи число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int result = number * number;
+System.Console.WriteLine (result);
---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введи число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 1)
+{
+    System.Console.WriteLine("Понедельн")
+}
+if(number == 1)
+{
+     System.Console.WriteLine("Вторник")
+}
+if(number == 1)
+{
+    System.Console.WriteLine("Среда")
+}

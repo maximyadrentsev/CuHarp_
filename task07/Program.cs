69d2d11b746c066ab3a68bb54f7928c373d34822
d <vf>
2023-10-08 22:48:06 +0500

    task07

---
+++ task07/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая принемает на вход трехзначное число и на выходе показывает последнию цифру этого числа.
+
+456 ->
+782 ->
+918 ->
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    System.Console.WriteLine("Error");
+}
+else
+{
+    number = (number % 10);
+    System.Console.WriteLine("последняя цифра " + number);
+}

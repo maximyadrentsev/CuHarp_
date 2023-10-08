5bc35d687538cf5c2a73a46355451333e2b2522b
d <vf>
2023-10-08 23:16:32 +0500

    дз4

---
+++ task08/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Внутри класса Answer напишите метод PrintEvenNumbers
+ которая на вход принимает число (number)
+ а на выходе выводит все чётные числа от 1 до number (включительно)
+разделеные знаком табуляции.
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i < number; i = i + 2)
+{
+    System.Console.Write(i + "\t");
+}

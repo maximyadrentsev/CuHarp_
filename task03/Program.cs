cdb9c16a333dc3b63beb9772d8e0b19142ea077c
d <vf>
2023-10-08 22:13:04 +0500

    task03

--- task03/Program.cs
+++ task03/Program.cs
@@ -2,13 +2,33 @@
 int number = Convert.ToInt32(Console.ReadLine());
 if(number == 1)
 {
-    System.Console.WriteLine("Понедельн")
+    System.Console.WriteLine("Понедельн");
 }
-if(number == 1)
+if(number == 2)
 {
-     System.Console.WriteLine("Вторник")
+     System.Console.WriteLine("Вторник");
 }
-if(number == 1)
+if(number == 3)
 {
-    System.Console.WriteLine("Среда")
+    System.Console.WriteLine("Среда");
+}
+if(number == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+if(number == 5)
+{
+   System.Console.WriteLine("Пятница");
+}
+if(number == 6)
+{
+    System.Console.WriteLine("Субота");
+}
+if(number == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}
+if(number >7 || number < 1)
+{
+    System.Console.WriteLine("Error");
 }

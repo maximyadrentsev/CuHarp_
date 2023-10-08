b270758c2a67b089b88234ca37bad0565edfeeed
d <vf>
2023-10-08 23:03:39 +0500

    четное число

---
+++ task06/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Внутри класса Answer напишите метод CompareNumbers
+который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 ==0)
+{
+    System.Console.WriteLine("Число четное ");
+}
+else 
+{
+    System.Console.WriteLine("Число не четное ");
+}

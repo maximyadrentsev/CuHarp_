775f21dba886d241d18988c8135982e98e7307f4
d <vf>
2023-10-11 21:15:31 +0500

    одно из чисел является квадратом

---
+++ task16/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принемает на вход два числа и проверяет,
+является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8, 9 -> нет
+*/
+System.Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2 || number2 == number1 * number1)
+{
+    System.Console.WriteLine("Одно из чисел квадрата другого");
+}
+else
+{
+    System.Console.WriteLine("не квадрат");
+}

c2cac5446192f3eca61f2a6c95da51d574088ac2
d <vf>
2023-10-11 18:12:42 +0500

    таск11

---
+++ task11/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.
+*/
+
+System.Console.WriteLine("Введите первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 7 == 0 & number % 23 == 0)
+{
+   System.Console.WriteLine("Да ");
+}
+else
+{
+    System.Console.WriteLine("Нет ");
+}

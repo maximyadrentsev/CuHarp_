ce2573ddcf9e35b7f9e29d1f5d7726760f7b4d7a
d <vf>
2023-10-11 17:55:29 +0500

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine(number);
+
+int first = number / 10; 
+int last = number % 10;
+
+if (first > last)
+{
+    System.Console.WriteLine("первоя больше ");
+}
+else if (last > first)
+{
+    System.Console.WriteLine("вторая больше ");
+}
+else
+{
+    System.Console.WriteLine("числа равны ");    
+}
